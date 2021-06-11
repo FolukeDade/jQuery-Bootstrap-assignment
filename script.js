57b937656d2d67a0b89eb86d4c1a47d0f7487e4d
Foluke Dade <folukedade@gmail.com>
2021-06-11 15:09:10 -0400

    started js-script

---
+++ script.js
@@ -0,0 +1,28 @@
+$( document ).ready(function() {
+
+    console.log( "ready!" );
+
+    $( ".changebtn1" ).click(function() {
+
+        console.log("change btn 1 clicked");
+        
+      });
+
+      $( ".changebtn2" ).click(function() {
+
+        console.log("change btn 2 clicked");
+        
+      });
+
+      $( ".changebtn3" ).click(function() {
+
+        console.log("change btn 3 clicked");
+        
+      });
+
+      $( ".changebtn4" ).click(function() {
+
+        console.log("change all clicked");
+        
+      });
+});
