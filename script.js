22aaee00a0b0636079c56b7009b3716776878ef8
Foluke Dade <folukedade@gmail.com>
2021-06-14 15:58:24 -0400

    updated change buttons

--- script.js
+++ script.js
@@ -12,19 +12,25 @@ $( document ).ready(function() {
       $( ".changebtn2" ).click(function() {
 
         console.log("change btn 2 clicked");
-        $(".box2").css({"background":"red","border":"2px solid blue"});
+        $(".box2").addClass("newbox2");
         
       });
 
       $( ".changebtn3" ).click(function() {
 
         console.log("change btn 3 clicked");
+        $(".box3").addClass("newbox3");
         
       });
 
-      $( ".changebtn4" ).click(function() {
+        $( ".changebtn4" ).click(function() {
 
         console.log("change all clicked");
+        $(".box1").css({"background":"red","border":"2px solid black"});
+        $(".box2").css({"background":"blue","border":"2px dashed yellow"});
+        $(".box3").css({"background":"green","border":"2px solid pink"});
         
-      });
+      }); 
+
+   
 });
