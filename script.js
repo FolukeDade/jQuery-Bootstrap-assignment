245923fcaf52b896e71a10b3b3ab1d32c31e04b7
Foluke Dade <folukedade@gmail.com>
2021-06-14 16:33:29 -0400

    updated hide buttons

--- script.js
+++ script.js
@@ -26,11 +26,48 @@ $( document ).ready(function() {
         $( ".changebtn4" ).click(function() {
 
         console.log("change all clicked");
-        $(".box1").css({"background":"red","border":"2px solid black"});
-        $(".box2").css({"background":"blue","border":"2px dashed yellow"});
-        $(".box3").css({"background":"green","border":"2px solid pink"});
+        $(".box1").css({"background":"purple","border":"2px dashed pink"});
+        $(".box2").css({"background":"grey","border":"2px solid black"});
+        $(".box3").css({"background":"cyan","border":"2px dashed red"});
         
       }); 
 
-   
+      $( ".hidebtn1" ).click(function() {
+
+        console.log("hide btn 1 clicked");
+        $(".box1").addClass("newbox4");
+        
+      });
+
+      $( ".hidebtn2" ).click(function() {
+
+        console.log("hide btn 2 clicked");
+        $(".box2").addClass("newbox4");
+        
+      });
+
+      $( ".hidebtn3" ).click(function() {
+
+        console.log("hide btn 3 clicked");
+        $(".box3").addClass("newbox4");
+        
+      });
+
+      $( ".resetcolors" ).click(function() {
+
+        console.log("change all clicked");
+        $(".box1").css({"background":"red","border":"2px solid black"});
+        $(".box2").css({"background":"blue","border":"2px dashed yellow"});
+        $(".box3").css({"background":"green","border":"2px solid purple"});
+        
+      }); 
+
+      $( ".changebtn4" ).click(function() {
+
+        console.log("change all clicked");
+        $(".box1").css({"background":"purple","border":"2px dashed pink"});
+        $(".box2").css({"background":"grey","border":"2px solid black"});
+        $(".box3").css({"background":"cyan","border":"2px dashed red"});
+        
+      });
 });
